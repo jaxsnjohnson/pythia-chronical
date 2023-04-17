110f72104cc5e1628bc64d7fe78508deb7a5110a
Jax SN Johnson <79726860+jaxsnjohnson@users.noreply.github.com>
2023-04-17 20:20:17 +0300

    Created core.sh
    This will be the core of this little app.

---
+++ core.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+echo "Welcome to the PC background generator!"
+echo "Please enter your name:"
+read name
+echo "Please enter your PC's name:"
+read pc_name
+echo "Thank you, $name! We will now generate a background for your PC, $pc_name."
