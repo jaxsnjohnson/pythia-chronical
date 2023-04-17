f77a08211ef28badc3575c5aa82c037027e50278
Jax SN Johnson <79726860+jaxsnjohnson@users.noreply.github.com>
2023-04-17 20:23:42 +0300

    Update core.sh
    Fixing error on 16 and 17 where it thought it was ambigious

--- core.sh
+++ core.sh
@@ -5,4 +5,15 @@ echo "Please enter your name:"
 read name
 echo "Please enter your PC's name:"
 read pc_name
-echo "Thank you, $name! We will now generate a background for your PC, $pc_name."
+
+# Get the current date and format it as yyyy-mm-dd
+date=$(date +'%Y-%m-%d')
+
+# Generate the filename using the naming convention
+filename="$date-$name-$pc_name.md"
+
+# Create the file and write the content
+echo "# $pc_name's Pythia Chronicle" > "$filename"
+echo "## $name $date" >> "$filename"
+
+echo "Thank you, $name! Your PC background has been generated and saved to $filename."
